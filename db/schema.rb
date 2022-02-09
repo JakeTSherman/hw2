2f6d17b711a5baf3b1681ea85eaa3ea3733935a6
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-02-09 02:45:17 +0000

    models created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_023505) do
+ActiveRecord::Schema.define(version: 2022_02_09_024239) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "year"
     t.string "rating"
     t.string "director"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "character_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
